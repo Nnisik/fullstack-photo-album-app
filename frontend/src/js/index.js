49e62865a65938652f72a36650883c71e816882c
Nina <nissik@Ninas-MacBook-Pro.local>
2026-02-19 00:46:44 +0100

    automised posts generation for profile page, removed blueprint post elems from index.html

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,3 +1,50 @@
+function createPostElement(postInfo, galleryPointer) {
+    const newPostDiv = document.createElement("div");
+    newPostDiv.classList.add("post");
+
+    const newPostImg = document.createElement("img");
+    newPostImg.classList.add("post-img");
+    // TODO: add href attribute
+    // TODO: add alt attribute
+    newPostDiv.appendChild(newPostImg);
+
+    const newPostDescripDiv = document.createElement("div");
+    newPostDescripDiv.classList.add("post-description");
+
+    const newPostHeader = document.createElement("h3");
+    newPostHeader.classList.add("post_text");
+    newPostHeader.innerText = postInfo.text;
+    newPostDescripDiv.appendChild(newPostHeader)
+
+    const likeBtnDiv = document.createElement("div");
+    likeBtnDiv.classList.add("post_like-btn");
+    // TODO: add pointers for a specific post
+
+    const likeBtn = document.createElement("input");
+    likeBtn.classList.add("like-btn");
+    likeBtn.type = "checkbox";
+    likeBtn.name = "like-btn";
+    likeBtnDiv.appendChild(likeBtn);
+    // TODO: add pointers for a specific post
+
+    const likeBtnCounter = document.createElement("p");
+    likeBtnCounter.classList.add("like-btn_counter");
+    likeBtnCounter.innerText = postInfo.likes_number;
+    likeBtnDiv.appendChild(likeBtnCounter);
+    newPostDescripDiv.appendChild(likeBtnDiv);
+    // TODO: add pointers for a specific post
+
+    newPostDiv.appendChild(newPostDescripDiv);
+    galleryPointer.appendChild(newPostDiv);
+}
+
+function drawNoPostsMessage(parentElementPointer) {
+    const emptyListMessage = document.createElement("p");
+    emptyListMessage.innerText = "Seems like you there are no posts yet";
+    // TODO: add styling for message text
+    parentElementPointer.appendChild(emptyListMessage);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
     // Noticing currently active page
@@ -13,6 +60,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 // FIXME
                 // TODO: connect function from diff files
                 const allUserPosts = getAllUserPosts(currentUserID);
+                if (length(allUserPosts) === 0) {
+                    drawNoPostsMessage(document.getElementById("profile_gallery"))
+                } else {
+                    allUserPosts.forEach((post) => {
+                        createPostElement(post, document.getElementById("profile_gallery"))
+                    });
+                }
+
             }
             catch (e) {
                 console.log(e);
