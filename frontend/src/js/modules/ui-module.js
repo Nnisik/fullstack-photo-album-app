ec64d4eb770c5bd67e7285cfef589bf897e34bd3
Nina <nissik@Ninas-MacBook-Pro.local>
2026-03-25 17:43:33 +0100

    removed old version of UI

--- frontend/src/js/modules/ui-module.js
+++
@@ -1,5 +0,0 @@
-export let showNoPostsMessage = () => {
-    let emptyMessageElement = document.createElement("p");
-    emptyMessageElement.innerText = "Ops, seems like you haven't posted anything yet...";
-    galleryContainer.appendChild(emptyMessageElement);
-}
